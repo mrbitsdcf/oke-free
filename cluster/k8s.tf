9dfca673f45c20f918c7ab14eaf09fd4bd40dbd1
Diogo Fernandes <diogo@nubeliu.com>
2024-01-29 10:04:44 -0300

    Revert ADS

--- cluster/k8s.tf
+++ cluster/k8s.tf
@@ -33,7 +33,7 @@ resource "oci_containerengine_node_pool" "k8s_node_pool" {
   name               = "k8s-node-pool"
   node_config_details {
     placement_configs {
-      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
       subnet_id           = var.vcn_private_subnet_id
     }
     placement_configs {
@@ -41,7 +41,7 @@ resource "oci_containerengine_node_pool" "k8s_node_pool" {
       subnet_id           = var.vcn_private_subnet_id
     }
     placement_configs {
-      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
       subnet_id           = var.vcn_private_subnet_id
     }
     size = var.node_size
